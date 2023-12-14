dfb8c21c1f8589cec5c1ee66284c82a8784a9a7a
Camilo Rodelo <camilo_jrl@hotmail.com>
2023-12-13 19:41:22 -0500

    navbar y menu desplegable superpuesto

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
 import Navbar from './components/navbar/Navbar'
 import Cards from './components/cards/Cards'
 import Login from './components/login/Login'
 import Footer from './Components/footer/Footer'
+import DropDownMenu from './components/dropDownMenu/DropDownMenu'
 
 function App() {
   return (
     <div>
+      <DropDownMenu />
       <Navbar />
-      <Login />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
       <Cards />
-      <Footer/>
+      <Footer />
     </div>
   )
 }
--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import Card from '../card/Card';
+import Card from '../card/Card'
 import productos from './books.json'
-
 
 function Cards() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 justify-items-center">
-
-      {productos.map(producto => (
+      {productos.map((producto) => (
         <div key={producto.id} class="p-4">
           <Card
             id={producto.id}
@@ -19,7 +17,7 @@ function Cards() {
         </div>
       ))}
     </div>
-  );
+  )
 }
 
 export default Cards
