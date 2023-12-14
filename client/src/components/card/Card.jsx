7b38e9e96fe7a887e6717d72b83c0cce25529591
NPirizSmith <nahuelpirizsmith2@gmail.com>
2023-12-14 17:55:25 -0500

    Get desde la api

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 import ButtonHeart from './ButtonHeart'
 import FiveStars from './FiveStars'
 
-function Card ({ id, imagen, titulo, autor, genero, sinopsis, editorial, ISBN }) {
+function Card ({ id, image, price, title, author, genre, synopsis, editorial, ISBN }) {
   return (
 
 <div class="relative w-full max-w-sm bg-white  border border-gray-200 rounded-lg shadow dark:border-gray-700">
     <a href="#">
       <div class="relative p-8">
         <ButtonHeart/>
-        <img class="rounded-lg  shadow-md" src={imagen} alt="product image" />
+        <img class="rounded-lg  shadow-md" src={image} alt="product image" />
     </div>
     </a>
     <div class="px-5 pb-5">
       
         <a href="#">
-            <h5 class="text-left text-xl font-semibold tracking-tight text-gray-900 dark:text-black">{titulo}</h5>
-            <h5 class="text-left text-xl font-semibold tracking-tight text-gray-900 dark:text-black">{genero}</h5>
-            <h5 class="text-left text-xl font-semibold tracking-tight text-gray-900 dark:text-black">{autor}</h5>
+            <h5 class="text-left text-xl font-semibold tracking-tight text-gray-900 dark:text-black">{title}</h5>
+            <h5 class="text-left text-xl font-semibold tracking-tight text-gray-900 dark:text-black">{genre}</h5>
+            <h5 class="text-left text-xl font-semibold tracking-tight text-gray-900 dark:text-black">{author}</h5>
         </a>
         <div class="flex items-center mt-2.5 mb-5">
             <FiveStars/>
             <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
         </div>
         <div class="flex items-center justify-between">
-            {/* <span class="text-left text-2xl font-bold text-gray-900 dark:text-black">Precio: {precio}</span> */}
+            <span class="text-left text-2xl font-bold text-gray-900 dark:text-black">Precio: {price}</span>
             <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Agregar al carrito</a>
         </div>
     </div>
--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
-import Card from '../card/Card'
-import productos from './books.json'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProducts } from '../../redux/services/getAllProducts';
+import Card from '../card/Card';
 
 function Cards() {
+  const dispatch = useDispatch();
+  const { list, loading, error } = useSelector((state) => state.products);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  
+  useEffect(() => {
+    dispatch(getProducts(currentPage));
+  }, [dispatch, currentPage]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 justify-items-center">
-      {productos.map((producto) => (
-        <div key={producto.id} class="p-4">
+      {list.map((product) => (
+        <div key={product.id} class="p-4">
           <Card
-            id={producto.id}
-            imagen={producto.imagen}
-            titulo={producto.titulo}
-            genero={producto.genero}
-            autor={producto.autor}
+            id={product.id}
+            image={product.image || 'Imagen no disponible'}
+            title={product.title || 'Título no disponible'}
+            genre={product.genre || 'Género no disponible'}
+            author={product.author || 'Autor no disponible'}
+            price={product.price || 'Precio no disponible'}
           />
         </div>
       ))}
     </div>
   )
 }
-
 export default Cards
