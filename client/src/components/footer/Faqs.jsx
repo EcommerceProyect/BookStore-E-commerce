96a0a8c374f776df84fd373658aa027b54f8a2fb
LuisaBarrios05 <luisabarrios01@gmail.com>
2023-12-14 22:51:49 -0300

    Cambio de estilos en Preguntas Frecuentes

--- client/src/components/footer/Faqs.jsx
+++ client/src/components/footer/Faqs.jsx
@@ -7,17 +7,17 @@ function Faqs (){
       <div class="mb-6 md:mb-0">
         <h2 class="mb-6 text-3xl font-bold">Frequently asked questions</h2>
 
-        <p class="text-neutral-500 dark:text-neutral-300">
+        <p class="mb-12 text-neutral-800">
           Didn't find your answer in the FAQ?
           <a href="#!"
-            class="text-primary transition duration-300 hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600">Contact
+            class="text-blue-500">Contact
             our sales team</a>.
         </p>
       </div>
 
       <div class="mb-6 md:mb-0">
         <p class="mb-4 font-bold">Anim pariatur cliche reprehenderit?</p>
-        <p class="mb-12 text-neutral-500 dark:text-neutral-300">
+        <p class="mb-12 text-neutral-800">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
           autem numquam dolore molestias aperiam culpa alias veritatis
           architecto eos, molestiae vitae ex eligendi libero eveniet
@@ -25,7 +25,7 @@ function Faqs (){
         </p>
 
         <p class="mb-4 font-bold">Non cupidatat skateboard dolor brunch?</p>
-        <p class="mb-12 text-neutral-500 dark:text-neutral-300">
+        <p class="mb-12 text-neutral-800">
           Distinctio corporis, iure facere ducimus quos consectetur ipsa ut
           magnam autem doloremque ex! Id, sequi. Voluptatum magnam sed fugit
           iusto minus et suscipit? Minima sunt at nulla tenetur, numquam
@@ -36,7 +36,7 @@ function Faqs (){
         <p class="mb-4 font-bold">
           Praesentium voluptatibus temporibus consequatur non aspernatur?
         </p>
-        <p class="mb-12 text-neutral-500 dark:text-neutral-300">
+        <p class="mb-12 text-neutral-800">
           Minima sunt at nulla tenetur, numquam unde quod modi magnam ab
           deserunt ipsam sint aliquid dolores libero repellendus cupiditate
           mollitia quidem dolorem.
