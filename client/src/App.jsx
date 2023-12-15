3b4c02bd3e8f12dd4f69365a20a8bfb74695d918
LuisaBarrios05 <luisabarrios01@gmail.com>
2023-12-14 22:54:48 -0300

    Agregué rutas Home y Preguntas frecuentes en App.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
 import Navbar from './components/navbar/Navbar'
-import Cards from './components/cards/Cards'
 import Login from './components/login/Login'
-import Footer from './Components/footer/Footer'
+import Footer from './components/footer/Footer'
+import Faqs from './components/footer/Faqs';
+import Home from './views/Home'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Login />
-      <Cards />
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/faqs" element={<Faqs />} />
+      </Routes>
       <Footer/>
     </div>
   )
