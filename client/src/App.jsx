0fa85c510c501e146c9e888b9a3bfb7f767f78a6
NPirizSmith <nahuelpirizsmith2@gmail.com>
2023-12-16 16:25:05 -0500

    Open y close Login modal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-
-import { Route, Routes } from 'react-router-dom';
-
-import Navbar from './components/navbar/Navbar'
-import Cards from './components/cards/Cards'
-import Login from './components/login/Login'
-import Footer from './Components/footer/Footer'
-import Registration from './components/registration/Registration'
-import CreateBook from './components/createBook/createBook'
+import React, { useState } from 'react';
+import Navbar from './components/navbar/Navbar';
+import Login from './components/login/Login';
+import Cards from './components/cards/Cards';
+import Footer from './Components/footer/Footer';
+import Registration from './components/registration/Registration';
+import CreateBook from './components/createBook/createBook';
+import DropDownMenu from './components/dropDownMenu/DropDownMenu';
 
 function App() {
+  const [showLoginModal, setShowLoginModal] = useState(false);
+
+  const openLoginModal = () => {
+    setShowLoginModal(true);
+  };
+
+  const closeLoginModal = () => {
+    setShowLoginModal(false);
+  };
+
   return (
     <div>
       <DropDownMenu />
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-
+      <Navbar openLoginModal={openLoginModal} /> {/* Pasar la función al Navbar */}
       <Cards />
       <Footer />
-
-
       <Registration />
       <CreateBook />
-
+      {showLoginModal && <Login onClose={closeLoginModal} />} {/* Mostrar el Login si showModal es true */}
     </div>
   );
 }
+
 export default App;
--- client/src/components/linkButtons/LinkButtons.jsx
+++ client/src/components/linkButtons/LinkButtons.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
 
-const Button = ({ link, text, icon }) => {
+const Button = ({ link, text, icon, onClick }) => {
   return (
     <Link to={link} className="flex flex-col items-center">
-      <button className="flex flex-col items-center p-2">{icon}</button>
+      <button onClick={onClick} className="flex flex-col items-center p-2">{icon}</button>
       <span className="text-black">{text}</span>
     </Link>
   )
