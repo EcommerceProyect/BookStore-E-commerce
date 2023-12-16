38f9ff5534ae20048327e9ddff714f705173eb49
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2023-12-16 19:06:25 -0300

    fix client 2

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/navbar/Navbar'
+import Cards from './components/cards/Cards'
+import Login from './components/login/Login'
+import Footer from './Components/footer/Footer'
+import Registration from './components/registration/Registration'
+import CreateBook from './components/createBook/createBook'
+
+function App() {
+  return (
+    <div>
+      <DropDownMenu />
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <Cards />
+      <Footer />
+
+
+      <Registration />
+      <CreateBook />
+
+    </div>
+  );
+}
+export default App;
