be072858e5b61ceaab44399cc7df9650d0c4ed5d
genac4 <genacura4@gmail.com>
2023-12-18 11:06:02 -0300

    Filtro por ISBN

--- API/src/controllers/FilterProductByISBNController.js
+++ API/src/controllers/FilterProductByISBNController.js
@@ -1,25 +1,33 @@
 const { Products, ISBN, Author, Editorial, Genre, ReleasedDate} = require("../db");
 
 const filterProductByISBNController = async (isbn) => {
-    const response = await Products.findOne({
-        include:[
-            {model: Author, as:"Author"},
-            {model: Editorial, as:"Editorial"},
-            {model: ReleasedDate, as:"ReleasedDate"},
-            {model: Genre, as: "Genre"},
-            {
-                model: ISBN,
-            as: "ISBN",
-            where:{
-            isbn: isbn
-            
-        }}
-        ]
-    });
-
-    return response;
+    try {
+        const response = await Products.findOne({
+            include:[
+                {model: Author, as:"Author"},
+                {model: Editorial, as:"Editorial"},
+                {model: ReleasedDate, as:"ReleasedDate"},
+                {model: Genre, as: "Genre"},
+                {
+                    model: ISBN,
+                as: "ISBN",
+                where:{
+                name: isbn
+            }}
+            ]
+        });
+    
+        if(!response){
+            throw new Error("Producto no encontrado")
+        }
+    
+        return response;
+    } catch (error) {
+        console.log("Error al filtrar por ISBN, error");
+        return error;
+    }
+    
 };
-
 
 module.exports = {
     filterProductByISBNController,
--- API/src/routes/indexRoutes.js
+++ API/src/routes/indexRoutes.js
@@ -2,13 +2,27 @@ const { Router } = require("express");
 const { getProducts } = require("../handlers/getProducts");
 const {createProduct} = require("../handlers/createProduct");
 const {filterProductByISBN} = require("../handlers/filterProductByISBN");
+const {filterProductByGenre} = require("../handlers/filterHandler/filterProductByGenre");
+const {filterProductByDate} = require("../handlers/filterHandler/filterProductByDate");
+const { getGenres } = require("../handlers/Products/getGenres");
+const { getAuthors } = require("../handlers/Products/getAuthors");
+const { getEditorials } = require("../handlers/Products/getEditorials");
+const { getISBNs } = require("../handlers/Products/getISBNs");
+
 const router = Router();
 
 router.post("/products",createProduct)
 
 router.get("/products",getProducts);
 
-//ACA TENDRIA QUE IR LA RUTA DEL FILTRO POR ISBN. Creo yo que seria algo asi.
+
+router.get("/products/filter",(req,res) =>{
+    const {rDate,genre} = req.query;
+    if(rDate)filterProductByDate(req,res);
+    else if(genre)filterProductByGenre(req,res);
+});
+
+//RUTA DEL FILTRO POR ISBN. Seria algo asi .
 router.get("/products/filter/:isbn", (req,res) => {
     const {isbn} = req.params;
     if (isbn) filterProductByISBN(req,res);
