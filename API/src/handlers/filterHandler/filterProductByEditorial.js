5ebe5ec22380db1b648fddc85df09977e6f5e63f
Christian Veliz <105735731+chrivel14@users.noreply.github.com>
2023-12-18 10:47:47 -0500

    Update filterProductByEditorial.js

--- API/src/handlers/filterHandler/filterProductByEditorial.js
+++ API/src/handlers/filterHandler/filterProductByEditorial.js
@@ -4,7 +4,7 @@ const {filterProductByEditorialController} = require("../../controllers/filterCo
 const filterProductByEditorial = async (req,res) =>{
     try {
         const editorial = req.query.editorial
-        const page = parseInt(req.query.page) || 1
+        const page = parseInt(req.query.page) 
         const response = await filterProductByEditorialController(editorial,page)
         res.status(200).json(response)
     } catch (error) {
