5c91526e9ca81985c066789fe2064f6743e7ca2f
LuisaBarrios05 <luisabarrios01@gmail.com>
2023-12-26 14:54:08 -0300

    Carousel en Home

--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Cards from '../../components/cards/Cards'
+import { Carousel } from 'flowbite-react'
 
 const Home = () => {
   return (
     <div>
+      <Carousel/>
       <Cards/>
     </div>
   )
