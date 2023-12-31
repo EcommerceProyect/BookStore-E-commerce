7132fb8a2272823ef0a0f2246afb30073d3f669e
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2023-12-30 22:22:38 -0300

    fix deploy

--- API/utilsAuth/API_Auth/auth.js
+++ API/utilsAuth/API_Auth/auth.js
@@ -9,6 +9,12 @@ const challengesAPIEndpoint = "https://api-books-auth0.onrender.com/authorized";
 
 
 app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(oAuth);
 
 //por params si viene una ruta se concatena para hacer la peticion correcta
