0db362db4d71935c11a7bcc82101b37b26b93757
LuisaBarrios05 <luisabarrios01@gmail.com>
2024-01-02 01:43:08 -0300

    Ajustes estilos

--- client/src/views/home/carousel/CardCarousel.jsx
+++ client/src/views/home/carousel/CardCarousel.jsx
@@ -9,8 +9,8 @@ const CardCarousel = ({id,
         <div class="flex flex-col justify-center h-full">
           <div
             class="h-full relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
-            <div class="w-full md:w-1/3 bg-white grid place-items-center">
-              <img src={image} alt="imagenCarrusel" class="rounded-xl" />
+            <div class="w-full md:w-1/3 bg-white grid place-items-center content-center">
+              <img src={image} alt="imagenCarrusel"/>
             </div>
             <div class="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
             <h3 class="font-black text-gray-800 md:text-3xl text-xl">{title}</h3>
