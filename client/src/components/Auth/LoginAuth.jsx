d03449e433eee0496173d7b92e4c623bfdab6501
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-01-04 15:12:25 -0300

    fix mi rama

--- client/src/components/Auth/LoginAuth.jsx
+++ client/src/components/Auth/LoginAuth.jsx
@@ -17,7 +17,7 @@ const LoginAuth = () => {
       if (challengesData === "none" && code && location.pathname !== "/redirect") {
         try {
           const response = await fetch(
-            `https://api-books-auth0.onrender.com/authorized?code=${code}&route=profile`,
+            `http://localhost:3001/authorized?code=${code}&route=profile`,
             {
               method: 'GET',
               headers: {
