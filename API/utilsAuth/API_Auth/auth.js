37e6e828bcf6a0115f46a4fff7c7f45b933c057f
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-01-04 01:49:24 -0300

    fix deploy 3

--- API/utilsAuth/API_Auth/auth.js
+++ API/utilsAuth/API_Auth/auth.js
@@ -10,7 +10,7 @@ const challengesAPIEndpoint = "https://bookstore-e-commerce.onrender.com/authori
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  // res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
