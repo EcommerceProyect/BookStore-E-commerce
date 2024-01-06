2dea71fbc931f5852867aad7a6ba3d5e8f2ad8c7
NPirizSmith <nahuelpirizsmith2@gmail.com>
2024-01-05 20:55:07 -0500

    funciona register

--- client/src/vars.js
+++ client/src/vars.js
@@ -3,5 +3,5 @@ export const AUDIENCE = 'https://www.protectAPI.com';
 export const SCOPE = 'admin:edit';
 export const CLIENT_ID = 'V1mOd1KV60WmMBdH9Lgw8vWWCEH7koDY';
 export const RESPONSE_TYPE = 'code';
-export const REDIRECT_URI = 'http://localhost:5173/';
+export const REDIRECT_URI = 'http://localhost:5173';
 export const APIDOMAIN = "http://localhost:3001"
