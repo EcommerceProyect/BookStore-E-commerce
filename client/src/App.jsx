747ac62295cc25ba0e37d88ff474e744d5eb1895
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-01-07 23:03:00 -0300

    fix deploy 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ import Users from './views/dashboard/Users';
 import CreateProduct from './views/createProduct/CreateProduct';
 import RegisterAuth from './components/Auth/RegisterAuth';
 import Products from './views/products/Products';
-import ProductList from './views/dashboard/productList';
+import ProductList from "./src/views/dashboard/productList";
 import PaymentBill from './views/cart/PaymentBill';
 import { useDispatch, useSelector } from 'react-redux';
 import { createCart } from './redux/slices/cartUsersTest';
