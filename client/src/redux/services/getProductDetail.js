352dd9b19d19946d41c3e0026e6f65e3aac89cb1
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-01-08 00:00:44 -0300

    fix deploy 7

--- client/src/redux/services/getProductDetail.js
+++ client/src/redux/services/getProductDetail.js
@@ -4,11 +4,13 @@ import {
   setProductDetailLoading,
   setProductDetailError,
 } from '../slices/products';
-
+import {
+  API_BOOKS
+} from '../../vars';
 
 
 export const getProductDetails = (id) => async (dispatch) => {
-  const apiUrl = `https://bookstore-e-commerce.onrender.com/ebook/products/${id}`;
+  const apiUrl = `${API_BOOKS}/ebook/products/${id}`;
   dispatch(setProductDetailLoading());
   try {
     const response = await axios.get(`${apiUrl}`);
