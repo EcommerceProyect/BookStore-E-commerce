43b7bbbbbbb4c6749435b1a0ce5508c9a368a934
MaximilianoDienes <maxdienes@gmail.com>
2024-01-13 16:32:00 -0300

    cambio en searchbar

--- client/src/views/dashboard/entitiesForm/AuthorForm.jsx
+++ client/src/views/dashboard/entitiesForm/AuthorForm.jsx
@@ -73,12 +73,6 @@ const AuthorForm = ({ setToast }) => {
             onChange={(e) => setSearchQuery(e.target.value)}
             required
           />
-          <button
-            type="submit"
-            class="text-white absolute end-2.5 bottom-2.5 bg-accents hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Buscar
-          </button>
         </div>
       </form>
 
