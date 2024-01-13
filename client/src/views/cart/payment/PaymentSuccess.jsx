6f72721c8a0291e43d6465f60ada52c0010b1c43
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-01-12 22:39:42 -0300

    arreglos reviews

--- client/src/views/cart/payment/PaymentSuccess.jsx
+++ client/src/views/cart/payment/PaymentSuccess.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { API_BOOKS } from '../../../vars';
-
 import {
   API_BOOKS,APIDOMAIN
 } from "../../../vars";
