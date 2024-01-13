b15900790ee688fb4895b749c6faac2c8ab6554d
genac4 <genacura4@gmail.com>
2024-01-12 21:57:41 -0300

    .

--- client/src/vars.js
+++ client/src/vars.js
@@ -1,7 +1,7 @@
 export const DOMAIN = 'dev-s3pcs1ovog464bay.us.auth0.com';
 
 export const AUDIENCE = 'https://www.protectAPI.com';
-export const SCOPE = 'user:edit';
+export const SCOPE = 'user:edit read:roles update:users create:role_members' ;
 export const CLIENT_ID = 'V1mOd1KV60WmMBdH9Lgw8vWWCEH7koDY';
 export const RESPONSE_TYPE = 'code';
 
@@ -11,4 +11,4 @@ export const RESPONSE_TYPE = 'code';
 
 export const REDIRECT_URI = 'http://localhost:5173';
 export const APIDOMAIN = 'http://localhost:3001';
-export const API_BOOKS = 'http://localhost:3002';
+export const API_BOOKS = 'http://localhost:5432';
