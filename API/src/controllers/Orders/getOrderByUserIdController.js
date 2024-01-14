673565c89ee59b448e253ca8c08dbe831e725b90
LuisaBarrios05 <luisabarrios01@gmail.com>
2024-01-14 03:13:02 -0300

    arreglo controller

--- API/src/controllers/Orders/getOrderByUserIdController.js
+++ API/src/controllers/Orders/getOrderByUserIdController.js
@@ -1,30 +1,37 @@
 /* eslint-disable no-undef */
-const {Orders, Cart,CartDetail} = require("../../db");
+const { Orders, Cart, CartDetail } = require("../../db");
 require("dotenv").config();
-const {LIMIT_USERS} = process.env;
+const { LIMIT_USERS } = process.env;
 
-const getOrderByUserIdController = async (id,page) => {
+const getOrderByUserIdController = async (id, page) => {
+  const offset = page * LIMIT_USERS;
 
-    const offset = page*LIMIT_USERS;
+  try {
+    const orders = await Orders.findAll({
+      where: {
+        "$Cart.UserId$": id
+      },
+      include: [Cart],
+      offset,
+      limit: LIMIT_USERS
+    });
 
-    try {
-        const orders = await Orders.findAll({ 
-            where:{
-                "$Cart.UserId$":id,
-            },
-            include: [Cart],
-            offset,
-            limit:LIMIT_USERS
+    const ordersProducts = await Promise.all(
+      orders.map(async (order) => {
+        const response = await CartDetail.findAll({
+          where: {
+            CartId: order.Cart.id
+          }
         });
 
         return { order, OrderDetail: response };
       })
     );
 
-        return ordersProducts;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+    return ordersProducts;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 module.exports = {
