67618ddab3827b43038f26681ce4cc9b2d9486fe
LuisaBarrios05 <luisabarrios01@gmail.com>
2024-01-14 17:07:27 -0300

    arreglo de importación del endpoint

--- client/src/redux/services/getOrderId.js
+++ client/src/redux/services/getOrderId.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_BOOKS } from '../../vars';
 export const getOrderId = async () =>{
       try {
-        const response = await axios.get('http://localhost:3002/ebook/orders?page=0');
+        const response = await axios.get(`${API_BOOKS}/ebook/orders?page=0`);
         return response;
       } catch (error) {
         console.log(error)
