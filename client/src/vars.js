4695f67eb6b7ade1e875c58e3697f0a2ef5d6d3d
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-01-16 22:06:30 -0300

    deploy fix 2

--- client/src/vars.js
+++ client/src/vars.js
@@ -4,12 +4,12 @@ export const SCOPE = 'user:edit read:roles update:users create:role_members';
 export const CLIENT_ID = 'V1mOd1KV60WmMBdH9Lgw8vWWCEH7koDY';
 export const RESPONSE_TYPE = 'code';
 
-// export const REDIRECT_URI = 'https://bookstore-e-commerce-z27y.onrender.com';
-// export const APIDOMAIN = 'https://api-books-auth0.onrender.com';
-// export const API_BOOKS = 'https://bookstore-e-commerce.onrender.com';
+export const REDIRECT_URI = 'https://bookstore-e-commerce-z27y.onrender.com';
+export const APIDOMAIN = 'https://api-books-auth0.onrender.com';
+export const API_BOOKS = 'https://bookstore-e-commerce.onrender.com';
 
-export const REDIRECT_URI = 'http://localhost:5173';
-export const APIDOMAIN = 'http://localhost:3001';
-export const API_BOOKS = 'http://localhost:5432';
+// export const REDIRECT_URI = 'http://localhost:5173';
+// export const APIDOMAIN = 'http://localhost:3001';
+// export const API_BOOKS = 'http://localhost:5432';
 
 export const ITEMS_PER_PAGE = 10;
