35d84c8358f8070f631fbb922ea62d4ea5b017e7
nicolaselozano <nicolas.lozano@alumnos.frm.utn.edu.ar>
2024-01-18 20:12:08 -0300

    vite-config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,16 +5,16 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   server: {
-  host: true,
-  strictPort: true,
-  port: 5173,
+    host: true,
+    strictPort: true,
+    port: 5173,
   },
   resolve: {
     alias: {
-      '/src/views/dashboard/Users.jsx': path.resolve(__dirname, 'src/views/dashboard/Users.jsx'),
-      '/src/views/dashboard/Statistics.jsx': path.resolve(__dirname, 'src/views/dashboard/Statistics.jsx'),
-      '/src/views/createProduct/CreateProduct.jsx': path.resolve(__dirname, 'src/views/createProduct/CreateProduct.jsx'),
-      '/src/views/dashboard/productList': path.resolve(__dirname, 'src/views/dashboard/productList'),
+      '@dashboard/Users': path.resolve(__dirname, 'src/views/dashboard/Users.jsx'),
+      '@dashboard/Statistics': path.resolve(__dirname, 'src/views/dashboard/Statistics.jsx'),
+      '@createProduct/CreateProduct': path.resolve(__dirname, 'src/views/createProduct/CreateProduct.jsx'),
+      '@dashboard/productList': path.resolve(__dirname, 'src/views/dashboard/productList'),
     },
   },
 })
